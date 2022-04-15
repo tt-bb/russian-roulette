3bb085fd56fd7064cefcd448f036cb142aaec663
tt-bb <ttbb_@outlook.com>
2022-04-16 00:17:18 +0200

    Russian roulette

---
+++ README.md
@@ -0,0 +1,7 @@
+# Russian Roulette
+
+Some small Python project. 
+
+---
+
+An implementation of the famous russian roulette.
---
+++ main.py
@@ -0,0 +1,24 @@
+import random
+import os
+import sys
+
+total_bullets = input("Please enter the number of chambers (default = 6): ")
+
+if not total_bullets:
+    total_bullets = 6
+
+elif not total_bullets.isdigit():
+    quit("Invalid number of chambers!")
+
+killer_bullet = random.randint(1, int(total_bullets))
+
+for x in range(1, int(total_bullets) + 1):
+    input("Press <ENTER> to pull the trigger!")
+    if x == killer_bullet:
+        print("YOU'RE DEAD !!\nGame Over")
+        start_again = input("Do you want to start again? (y/n):(default = yes))")
+        if not (start_again and not (start_again.lower()[0] == "y")):
+            os.execv(sys.executable, [sys.executable] + sys.argv)
+        else:
+            break
+    print("You're LUCKY'\n \n -----")
